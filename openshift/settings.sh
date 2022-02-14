47b1e54bce0b07083223f297cfcd10ed0b95b4bf
wadeking98 <wkingnumber2@gmail.com>
2022-02-14 10:34:22 -0800

    Initial Commit
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

---
+++ openshift/settings.sh
@@ -0,0 +1,9 @@
+export PROJECT_NAMESPACE="4a9599"
+export GIT_URI="https://github.com/bcgov/aries-vcr.git"
+export GIT_REF="master"
+
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export skip_git_overrides="schema-spy-build.yaml search-engine-base-build.yaml backup-build.yaml"
+export ignore_templates="offline-indexer-deploy frontend-v2-deploy"
